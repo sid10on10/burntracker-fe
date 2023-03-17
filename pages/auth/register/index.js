9db99c78ba317ccc2f2499cdd2f2d50e0ba64537
Aditya Hada <apshada1@gmail.com>
2023-03-17 09:20:19 +0530

    added register

--- pages/auth/register/index.js
+++ pages/auth/register/index.js
@@ -1,5 +1,6 @@
 import { Box, Button, Grid, TextField } from '@mui/material';
 import { useEffect, useState } from 'react';
+import  axios from 'axios';
 
 import styles from '../../../styles/register/Register.module.css';
 
@@ -10,6 +11,38 @@ export default function Register(){
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
 
+    const  handleRegister = () => {
+        if(validateEmail(email) && first.length > 0 && last.length > 0){
+            console.log({
+                "name": first + " " + last,
+                "email": email,
+                "password": password
+            })
+
+            const headers = {
+                "Content-Type": "application/json",
+              };
+           
+            axios.post("http://localhost:6000/register", {
+                "name": first + " " + last,
+                "email": email,
+                "password": password
+            })
+            .then((response) => {
+                console.log(response);
+            });
+        }
+
+        
+    }
+
+    const validateEmail = (email) => {
+        return String(email)
+          .toLowerCase()
+          .match(
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          );
+      };
     return (
         <Box>
             <Grid container>
@@ -20,7 +53,7 @@ export default function Register(){
                             <p className={styles.para}>Please fill your detail to create your account.</p>
                             <Box sx={{ marginTop: '20px' }}>
                                 <p className={styles.label}>First Name</p>
-                                <TextField id="outlined-firstname" type="text" variant="outlined"  sx={{ 
+                                <TextField id="outlined-firstname" type="text" variant="outlined" onChange={() => setFirst(event.target.value)}  sx={{ 
                                         width: '50%',  
                                         '& .MuiOutlinedInput-root': {
                                             '&.Mui-focused fieldset': {
@@ -35,7 +68,9 @@ export default function Register(){
                             </Box>
                             <Box sx={{ marginTop: '20px' }}>
                                 <p className={styles.label}>Last Name</p>
-                                <TextField id="outlined-lastname" type="text" variant="outlined"  sx={{ 
+                                <TextField id="outlined-lastname" type="text" variant="outlined" 
+                                onChange={() => setLast(event.target.value)}
+                                sx={{ 
                                         width: '50%',  
                                         '& .MuiOutlinedInput-root': {
                                             '&.Mui-focused fieldset': {
@@ -50,7 +85,9 @@ export default function Register(){
                             </Box>
                             <Box sx={{ marginTop: '20px' }}>
                                 <p className={styles.label}>Email</p>
-                                <TextField id="outlined-email" type="email" variant="outlined"  sx={{ 
+                                <TextField id="outlined-email" type="email" variant="outlined" 
+                                onChange={() => setEmail(event.target.value)}
+                                sx={{ 
                                         width: '50%',  
                                         '& .MuiOutlinedInput-root': {
                                             '&.Mui-focused fieldset': {
@@ -65,7 +102,9 @@ export default function Register(){
                             </Box>
                             <Box sx={{ marginTop: '20px' }}>
                                 <p className={styles.label}>Password</p>
-                                <TextField id="outlined-firstname" type="password" variant="outlined"  sx={{ 
+                                <TextField id="outlined-firstname" type="password" variant="outlined"  
+                                onChange={() => setPassword(event.target.value)}
+                                sx={{ 
                                         width: '50%',  
                                         '& .MuiOutlinedInput-root': {
                                             '&.Mui-focused fieldset': {
@@ -78,7 +117,7 @@ export default function Register(){
                                     }} 
                                 />                            
                             </Box>
-                            <Box sx={{ marginTop: '20px' }}>
+                            <Box sx={{ marginTop: '20px' }} onClick={handleRegister}>
                                 <Button variant="contained" sx={{ backgroundColor: '#FF4500', width: '50%', height: '44px', textTransform: 'capitalize' }}>Register</Button>
                             </Box>
                             <Box>
